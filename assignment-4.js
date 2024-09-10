417d8fc53c4bec81e5641bef4e1f2d67a35b4f2f
MD. Saharior Ridoy <emonhridoy2014@gmail.com>
2024-09-11 01:06:19 +0600

    Solved 2

--- assignment-4.js
+++ assignment-4.js
@@ -17,3 +17,20 @@ return totalTax;
 
 
 
+
+
+/*Problem-2 */
+function sendNotification(email) {
+    
+    if(typeof email !== 'string' || !email.includes('@')){
+        return "Invalid Email"  
+    }
+    const splittedArray = email.split('@');
+    const userName =splittedArray[0];
+    const domainName =splittedArray[1];
+    const notificationMessage=userName+" sent you an email from "+domainName;
+    return notificationMessage;
+}
+
+// console.log(sendNotification("zihad@gmail.com"));
+
